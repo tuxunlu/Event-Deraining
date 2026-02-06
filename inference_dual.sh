30bf6479ea15da96fc1db6cbe4a39aae55293b6f
Tuxun Lu <tuxunlu@umd.edu>
2026-02-06 14:28:32 -0500

    Sync progress

--- inference_dual.sh
+++ inference_dual.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python inference_dual.py \
 --config_path /fs/nexus-scratch/tuxunlu/git/event-based-deraining/lightning_logs/20251210-02-17-23-DualStreamFourierMamba_bg+rain_losses/version_0/hparams.yaml \
---test_checkpoint /fs/nexus-scratch/tuxunlu/git/event-based-deraining/lightning_logs/20251210-02-17-23-DualStreamFourierMamba_bg+rain_losses/version_0/checkpoints/best-epoch=034-val_loss_epoch=5.81781.ckpt \
+--test_checkpoint /fs/nexus-scratch/tuxunlu/git/event-based-deraining/lightning_logs/20251210-02-17-23-DualStreamFourierMamba_bg+rain_losses/version_0/checkpoints/best-epoch=049-val_loss_epoch=5.71666.ckpt \
 --no-save
