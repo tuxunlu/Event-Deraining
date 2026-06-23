e26b37060a6ef2716de58cd653e728d1e2d0290a
Tuxun Lu <tuxunlu@umd.edu>
2026-06-23 09:52:10 -0400

    Refactor inference and model scripts; update environment variables
    - Updated PYTHONPATH in activate.sh to reflect new project structure.
    - Enhanced inference.py with new functions for parameter counting and GFLOPs estimation, along with improved metric printing.
    - Modified inference.sh to use updated configuration and checkpoint paths.
    - Updated main.py to support a new model interface for point-based models.
    - Refactored loss function in model_interface.py to introduce a new joint objective for improved training performance.

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python inference.py \
-  --config_path "/fs/nexus-scratch/tuxunlu/git/Event-Deraining/configs/config_dynamicfourierfilternet.yaml" \
-  --test_checkpoint "/fs/nexus-scratch/tuxunlu/git/Event-Deraining/lightning_logs/20260223-12-46-21-DynamicFourierFilterNet_2D/version_0/checkpoints/best-epoch=049-val_loss_epoch=2.26851.ckpt" \
-  --no-save
+  --config_path "/fs/nexus-scratch/tuxunlu/git/Event-Deraining/lightning_logs/20260225-00-22-10-DynamicFourierFilterNetFDConv_2D/version_0/hparams.yaml" \
+  --test_checkpoint "/fs/nexus-scratch/tuxunlu/git/Event-Deraining/lightning_logs/20260225-00-22-10-DynamicFourierFilterNetFDConv_2D/version_0/checkpoints/best-epoch=049-val_loss_epoch=2.21635.ckpt" \
+  # --no-save
